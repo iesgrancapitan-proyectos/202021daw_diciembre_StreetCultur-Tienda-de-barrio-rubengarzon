8f152bd2e157819772daa96fb7c8d829476f7b57
Rubén Garzón <rubengarzonzafra@gmail.com>
2021-09-22 18:12:44 +0200

    componente menu creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      MainComponent
+      MainComponent,
+      MenuComponent
    ],
   imports: [
     BrowserModule,
