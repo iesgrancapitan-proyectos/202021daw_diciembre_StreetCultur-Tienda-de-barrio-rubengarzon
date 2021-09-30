c06c5233f11e8dec7809f08be0fedef47d6000ef
Rubén Garzón <rubengarzonzafra@gmail.com>
2021-09-30 11:00:07 +0200

    slider añadido

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -6,13 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.sass'],
 })
 export class BodyComponent implements OnInit {
-  src = '../../assets/prueba.jpg';
+
+  idImagen = 1;
 
   constructor() {}
 
   ngOnInit() {}
-
-   cambiarImagen() {
-    return this.src;
+  /**
+   * Retroceder imagen del slider
+   */
+  retrocederImagen(){
+    if (this.idImagen == 1) {
+      this.idImagen = 3;
+    } else if (this.idImagen == 2) {
+      this.idImagen = 1;
+    } else if (this.idImagen == 3) {
+      this.idImagen = 2;
+    }
+  }
+  /**
+   * Avanzar imagen del slider
+   */
+  avanzarImagen(){
+    if (this.idImagen == 1) {
+      this.idImagen = 2;
+    }else if(this.idImagen == 2){
+      this.idImagen = 3;
+    }else if(this.idImagen == 3){
+      this.idImagen = 1;
+    }
   }
 }
