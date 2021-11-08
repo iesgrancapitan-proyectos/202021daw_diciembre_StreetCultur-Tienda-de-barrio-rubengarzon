ffd60c135152f0e751399eb4be005680364b0c30
Rubén Garzón <rubengarzonzafra@gmail.com>
2021-11-08 19:37:32 +0100

    añadir producto al carro modificado

--- src/app/Client/sudaderas/sudaderas.component.ts
+++ src/app/Client/sudaderas/sudaderas.component.ts
@@ -35,7 +35,16 @@ export class SudaderasComponent implements OnInit {
       let id = sessionStorage.getItem('id');
       let carrito = { Nombre: nombre, Precio: precio, Id: id };
       this.carro.insertarCarro(carrito).subscribe( dato => {
-        console.log(dato);
+       if (Object.values(dato).includes("OK") == true){
+        return this.snackBar.open('Se ha añadido al carrito.', '', {
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+          duration: 1500,
+        });
+       }else{
+         console.log(dato);
+         return false;
+       }
       });
       return false;
     } else {
