afe21115866f429a8e789b942c88508c34c356d7
Rubén Garzón <rubengarzonzafra@gmail.com>
2021-11-18 13:30:29 +0100

    header modificado

--- src/app/Client/sudaderas/sudaderas.component.ts
+++ src/app/Client/sudaderas/sudaderas.component.ts
@@ -4,6 +4,8 @@ import { RopaService } from 'src/app/ropa.service';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { CarroService } from 'src/app/carro.service';
 import { LoginComponent } from 'src/app/main/login/login.component';
+import { LoginService } from 'src/app/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sudaderas',
@@ -22,7 +24,9 @@ export class SudaderasComponent implements OnInit {
     private login: LoginComponent,
     readonly snackBar: MatSnackBar,
     private carro: CarroService,
-    private carritoServicio: CarroService
+    private carritoServicio: CarroService,
+    private loginService: LoginService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -76,6 +80,36 @@ export class SudaderasComponent implements OnInit {
     let id1 = { Id: id };
     this.carritoServicio.contarProductos(id1).subscribe((dato: any) => {
       this.numProductos = Object.values(dato['numero'][0]);
+    });
+  }
+
+  /**
+   * Inicia sesión el empleado y almacena el email en una sesion
+   */
+   loginEmail() {
+    this.loginService.loginUsuario(this.login).subscribe((datos: any) => {
+      if (datos['resultado'] == 'OK') {
+        this.router.navigateByUrl('/');
+        sessionStorage.setItem('email', datos.email);
+        sessionStorage.setItem('id', datos.id);
+        this.loginService.comprobarPerfil().subscribe((datos) => {
+          switch (datos['perfil']) {
+            case 'cliente':
+              this.router.navigate(['/']);
+              break;
+            case 'empleado':
+              this.router.navigate(['/empleado']);
+              break;
+            case 'admin':
+              this.router.navigate(['/admin']);
+              break;
+            default:
+              break;
+          }
+        })
+      } else {
+        console.log('Ha habido un error al iniciar sesión');
+      }
     });
   }
 }
