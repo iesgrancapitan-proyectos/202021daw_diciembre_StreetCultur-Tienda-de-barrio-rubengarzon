07f5eae703c9ead5dc48b155d2a5cb1a11c3f06f
Rubén Garzón <rubengarzonzafra@gmail.com>
2021-11-18 10:38:30 +0100

    detalle sudadera modificado

--- src/app/Client/sudaderas/detalle-sudadera/detalle-sudadera.component.ts
+++ src/app/Client/sudaderas/detalle-sudadera/detalle-sudadera.component.ts
@@ -4,7 +4,7 @@ import { CarroService } from 'src/app/carro.service';
 import { LoginComponent } from 'src/app/main/login/login.component';
 import { RopaService } from 'src/app/ropa.service';
 import { ActivatedRoute } from '@angular/router';
-import { identifierModuleUrl } from '@angular/compiler';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-detalle-sudadera',
@@ -27,17 +27,22 @@ export class DetalleSudaderaComponent implements OnInit {
 
   numProductos: any;
 
+  cantidad: any
+
   estaLogueado: boolean = this.login.estaLogueado();
 
   constructor(private login: LoginComponent,
               private carritoServicio: CarroService,
               private ropaServicio: RopaService,
-              private rutaActiva: ActivatedRoute
+              private rutaActiva: ActivatedRoute,
+              readonly snackBar: MatSnackBar,
+              private carro: CarroService,
               ) { }
 
   ngOnInit() {
     this.contarProductos();
     this.mostrarSudadera(this.rutaActiva.snapshot.params.id);
+    this.cantidad = 1;
   }
 
   cerrarSesion() {
@@ -59,8 +64,41 @@ export class DetalleSudaderaComponent implements OnInit {
       Id: id
     }
     this.ropaServicio.obtenerSudadera(sudadera).subscribe((datos: any) => {
+      this.sudaderas.Nombre = Object.values(datos)[0][0]['Nombre'];
+      this.sudaderas.Precio = Object.values(datos)[0][0]['Precio'];
+      this.sudaderas.Descripcion = Object.values(datos)[0][0]['Descripcion'];
       this.sudaderas.Imagen = Object.values(datos)[0][0]['Imagen'];
+      this.sudaderas.Color = Object.values(datos)[0][0]['Color'];
     });
+  }
+
+  addCarrito(nombre: any, precio: any, imagen: any) {
+    if (sessionStorage.getItem('email')) {
+
+      let id = sessionStorage.getItem('id');
+      let carrito = { nombre: nombre, imagen: imagen, precio: precio, id: id };
+
+      this.carro.insertarCarro(carrito).subscribe( dato => {
+       if (Object.values(dato).includes("OK") == true){
+        this.contarProductos();
+
+        return this.snackBar.open('Se ha añadido al carrito.', '', {
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+          duration: 1500,
+        });
+       }else{
+         console.log(dato);
+         return false;
+       }
+      });
+      return false;
+    } else {
+      return this.snackBar.open('Necesitas iniciar sesión.', 'De acuerdo', {
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+      });
+    }
   }
 
 }
--- src/app/main/body/body.component.ts
+++ src/app/main/body/body.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { CarroService } from 'src/app/carro.service';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-body',
@@ -7,12 +9,34 @@ import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
   styleUrls: ['./body.component.sass'],
 })
 export class BodyComponent implements OnInit {
-  constructor(readonly snackBar: MatSnackBar) {}
+  panelOpenState = false;
+  numProductos: any;
+
+  estaLogueado: boolean = this.login.estaLogueado();
+
+  constructor(private login: LoginComponent,readonly snackBar: MatSnackBar, private carritoServicio: CarroService) {
+
+  }
 
   ngOnInit() {
+    this.contarProductos();
     return this.snackBar.open('Este sitio web es un proyecto de FP que está en desarrollo.', 'De acuerdo', {
       horizontalPosition: 'center',
       verticalPosition: 'bottom'
     });
+  }
+
+  contarProductos() {
+    let id = sessionStorage.getItem('id');
+    let id1 = { Id: id };
+    this.carritoServicio.contarProductos(id1).subscribe((dato: any) => {
+      this.numProductos = Object.values(dato['numero'][0]);
+    });
+  }
+
+  cerrarSesion() {
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('id');
+    this.estaLogueado = false;
   }
 }
