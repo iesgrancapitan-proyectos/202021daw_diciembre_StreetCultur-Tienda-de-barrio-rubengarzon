b926ec0d1af182015bff7af41a516c6fa08cd4ab
Rubén Garzón <rubengarzonzafra@gmail.com>
2021-11-23 13:36:32 +0100

    gestionar clientes

--- src/app/admin/gestionar-clientes/gestionar-clientes.component.ts
+++ src/app/admin/gestionar-clientes/gestionar-clientes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ClienteService } from 'src/app/cliente.service';
 import { LoginComponent } from 'src/app/main/login/login.component';
 import { Cliente } from 'src/app/Model/Cliente';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-gestionar-clientes',
@@ -13,9 +14,24 @@ export class GestionarClientesComponent implements OnInit {
 
   clientes: Cliente[] = [];
 
+  clientes1 = {
+    id: null,
+    perfil: null,
+    email: null,
+    password: null,
+    nombre: null,
+    apellidos: null,
+    provincia: null,
+    localidad: null,
+    domicilio: null,
+    codigopostal: null,
+    movil: null
+  }
+
   constructor(
     private login: LoginComponent,
     private clienteServicio: ClienteService,
+    readonly snackBar: MatSnackBar
   ) {}
 
   ngOnInit() {
@@ -30,4 +46,44 @@ export class GestionarClientesComponent implements OnInit {
     this.estaLogueado = false;
   }
 
+  pasarDatos(email){
+    let email1 = {
+      email: email
+    }
+    this.clienteServicio.mostrarCliente(email1).subscribe((datos)=> {
+      this.clientes1.id = datos["cliente"][0]["id"];
+      this.clientes1.perfil = datos["cliente"][0]["perfil"];
+      this.clientes1.email = datos["cliente"][0]["email"];
+      this.clientes1.nombre = datos["cliente"][0]["nombre"];
+      this.clientes1.apellidos = datos["cliente"][0]["apellidos"];
+      this.clientes1.provincia = datos["cliente"][0]["provincia"];
+      this.clientes1.localidad = datos["cliente"][0]["localidad"];
+      this.clientes1.domicilio = datos["cliente"][0]["domicilio"];
+      this.clientes1.codigopostal = datos["cliente"][0]["codigopostal"];
+      this.clientes1.movil = datos["cliente"][0]["movil"];
+    })
+  }
+
+  modificarCliente(id,perfil,email,nombre,apellidos,provincia,localidad,domicilio,codigopostal,movil){
+    let cliente2 = {
+      id: id,
+      perfil:perfil,
+      email:email,
+      nombre:nombre,
+      apellidos:apellidos,
+      provincia:provincia,
+      localidad:localidad,
+      domicilio:domicilio,
+      codigopostal:codigopostal,
+      movil:movil
+    }
+    this.clienteServicio.actualizarCliente(cliente2).subscribe((datos) => {
+      if(datos["resultado"] == "OK"){
+        this.snackBar.open('El cliente se ha modificado', '', {
+          duration: 2000,
+        });
+        this.clienteServicio.mostrarClientes();
+      }
+    })
+  }
 }
