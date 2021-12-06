e12e224b61b3161c539dd50f81dadc0e2db55640
Rubén Garzón <rubengarzonzafra@gmail.com>
2021-12-06 20:56:31 +0100

    texto corregido

--- src/app/Employee/empleado/empleado.component.ts
+++ src/app/Employee/empleado/empleado.component.ts
@@ -3,22 +3,32 @@ import { Router } from '@angular/router';
 import { LoginService } from 'src/app/login.service';
 import { LoginComponent } from 'src/app/main/login/login.component';
 import { ClienteService } from 'src/app/cliente.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.sass']
+  styleUrls: ['./empleado.component.sass'],
 })
 export class EmpleadoComponent implements OnInit {
   showFiller = false;
-  nombreEmpleado:any;
+  nombreEmpleado: any;
   estaLogueado: boolean;
 
-
-  constructor(private clienteServicio: ClienteService,private login: LoginComponent,private loginServicio: LoginService, private router: Router) { }
+  constructor(
+    private clienteServicio: ClienteService,
+    private login: LoginComponent,
+    readonly snackBar: MatSnackBar,
+    private loginServicio: LoginService,
+    private router: Router
+  ) {}
 
   tiempoTranscurrido = Date.now();
   hoy = new Date(this.tiempoTranscurrido);
+
+  form1: FormGroup;
 
   cliente = {
     id: sessionStorage.getItem('id'),
@@ -32,56 +42,82 @@ export class EmpleadoComponent implements OnInit {
     domicilio: null,
     codigopostal: null,
     movil: null,
-    imagen: null
+    imagen: null,
   };
 
   ngOnInit() {
     this.obtenerDatos();
 
-    console.log(this.cliente.imagen)
+    console.log(this.cliente.imagen);
 
-    this.loginServicio.comprobarPerfil().subscribe((datos) =>{
-      this.nombreEmpleado = datos["nombre"]
-    })
+    this.loginServicio.comprobarPerfil().subscribe((datos) => {
+      this.nombreEmpleado = datos['nombre'];
+    });
 
-    if(sessionStorage.getItem("email")){
+    this.form1 = new FormGroup({
+      id: new FormControl(),
+      nombre: new FormControl(),
+      apellidos: new FormControl(),
+      provincia: new FormControl(),
+      localidad: new FormControl(),
+      imagen: new FormControl(),
+    });
+
+    if (sessionStorage.getItem('email')) {
       this.estaLogueado = true;
-    }else{
+    } else {
       this.estaLogueado = false;
     }
 
-    if(sessionStorage.getItem("email").length > 0){
-    this.loginServicio.comprobarPerfil().subscribe((datos) => {
-        if(datos["perfil"] == "cliente"){
-          this.router.navigate(["/"]);
-        }else if(datos["perfil"] == "admin"){
-          this.router.navigate(["/admin"]);
+    if (sessionStorage.getItem('email').length > 0) {
+      this.loginServicio.comprobarPerfil().subscribe((datos) => {
+        if (datos['perfil'] == 'cliente') {
+          this.router.navigate(['/']);
+        } else if (datos['perfil'] == 'admin') {
+          this.router.navigate(['/admin']);
         }
-      })
-    }else{
-      this.router.navigate(["/login"]);
+      });
+    } else {
+      this.router.navigate(['/login']);
     }
 
     console.log(this.estaLogueado);
   }
+
+  actualizarInfo() {
+    console.log(this.form1.value);
+    this.clienteServicio
+      .actualizarCliente(this.form1.value)
+      .subscribe((datos) => {
+        if (datos['resultado'] == 'OK') {
+          this.snackBar.open('Se ha actualizado la información', '', {
+            duration: 2000,
+          });
+        } else {
+          this.snackBar.open('Error inesperado', '', {
+            duration: 2000,
+          });
+        }
+      });
+  }
+
   obtenerDatos() {
     let email = sessionStorage.getItem('email');
 
     let email1 = { email: email };
 
     this.clienteServicio.mostrarCliente(email1).subscribe((datos) => {
-      console.log(datos['cliente'][0]['perfil'])
-        this.cliente.perfil = datos['cliente'][0]['perfil'];
-        this.cliente.nombre = datos['cliente'][0]['nombre'];
-        this.cliente.apellidos = datos['cliente'][0]['apellidos'];
-        this.cliente.provincia = datos['cliente'][0]['provincia'];
-        this.cliente.localidad = datos['cliente'][0]['localidad'];
-        this.cliente.domicilio = datos['cliente'][0]['domicilio'];
-        this.cliente.codigopostal = datos['cliente'][0]['codigopostal'];
-        this.cliente.movil = datos['cliente'][0]['movil'];
-        this.cliente.imagen = datos['cliente'][0]['imagen'];
-      }
-    );
+      console.log(datos['cliente'][0]['perfil']);
+      this.cliente.perfil = datos['cliente'][0]['perfil'];
+      this.cliente.nombre = datos['cliente'][0]['nombre'];
+      this.cliente.apellidos = datos['cliente'][0]['apellidos'];
+      this.cliente.provincia = datos['cliente'][0]['provincia'];
+      this.cliente.localidad = datos['cliente'][0]['localidad'];
+      this.cliente.domicilio = datos['cliente'][0]['domicilio'];
+      this.cliente.codigopostal = datos['cliente'][0]['codigopostal'];
+      this.cliente.movil = datos['cliente'][0]['movil'];
+      this.cliente.imagen = datos['cliente'][0]['imagen'];
+    });
   }
   cerrarSesion() {
     sessionStorage.removeItem('email');
